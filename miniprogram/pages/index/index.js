c3f597e2febb57408ece9ecc7d1ea215c7474623
wangkj <>
2025-03-21 22:43:37 +0800

    add result-detail

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,41 +1,5 @@
 import dayjs from 'dayjs'
-import lunisolar from 'lunisolar'
-import {
-  analyzeWuXing
-} from '../../lib/suanming/index'
-import Toast from '@vant/weapp/toast/toast'
-import * as echarts from '../../ec-canvas/echarts';
 
-
-let chart = null;
-
-function initChart(canvas, width, height, dpr) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
-
-  const option = {
-    xAxis: {
-      type: 'category',
-      data: ['金', '木', '水', '火', '土']
-    },
-    yAxis: {
-      type: 'value',
-      show: false
-    },
-    series: [{
-      data: [0, 0, 0, 0, 0],
-      type: 'bar'
-    }]
-  };
-
-
-  chart.setOption(option);
-  return chart;
-}
 
 Page({
 
@@ -45,7 +9,7 @@ Page({
   data: {
     calendarTypeList: ['农历', '阳历'],
     calendarType: '农历',
-    selectedDate: '请选择日期',
+    selectedDate: '1992-12-01',
     chineseHours: [
       '子时 (23:00-1:00)',
       '丑时 (1:00-3:00)',
@@ -60,24 +24,13 @@ Page({
       '戌时 (19:00-21:00)',
       '亥时 (21:00-23:00)'
     ],
-    chineseHour: '请选择时辰',
-    ec: {
-      onInit: initChart
-    }
+    chineseHour: '辰时 (7:00-9:00)'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    console.log(`异步打印----dayjs('1900-01-01 00:00').valueOf(): `, dayjs('1900-01-01 00:00').valueOf())
-    wx.showShareMenu({
-      withShareTicket: true,
-      menus: ['shareAppMessage', 'shareTimeline'],
-      success: () => console.log('分享菜单已调起'),
-      fail: () => console.log('分享菜单已关闭')
-    });
-  },
+  onLoad(options) {},
 
   onChangeCalendarType(e) {
     const i = +e.detail.value
@@ -103,74 +56,10 @@ Page({
     })
   },
 
-  onSelectCalendarType(e) {
-    console.log(`异步打印----e.detail: `, e.detail)
-    const {
-      name
-    } = e.detail
-    this.setData({
-      calendarType: name,
-      showCalendarType: false
-    })
-  },
-
-  onCloseCalendarType(e) {
-    // console.log(`异步打印----e.detail: `, e.detail)
-    this.setData({
-      showCalendarType: false
-    })
-
-  },
-
-  onChangeNongli() {
-    this.setData({
-      showNongli: !this.data.showNongli
-    })
-  },
-
-  onCloseCalendar() {
-    this.setData({
-      showCalendar: false
-
-    })
-
-  },
-
-  onCloseCalendarActionSheet() {
-    this.setData({
-      showCalendar: false
-    })
-  },
-  onConfirmCalendar(e) {
-    console.log(`异步打印----e.detail: `, e.detail)
-    this.setData({
-      // currentDate: false
-
-    })
-
-  },
-  onInputDate(e) {
-    console.log(`选择的时间戳: `, e.detail)
-    const date1 = new Date(e.detail)
-    this.setData({
-      birthStr: dayjs(e.detail).format('YYYY-MM-DD HH:mm'),
-      year: date1.getFullYear(),
-      month: date1.getMonth() + 1,
-      day: date1.getDate(),
-      hour: date1.getHours(),
-      minute: date1.getMinutes(),
-      showCalendar: false
-    })
-  },
-
-  handleShare () {
-    console.log(`异步打印----fenxiang: `, )
-  },
-
   queryBazi() {
 
 
-    if (!(/^\d/.test(this.data.birthStr))) {
+    if (!(/^\d/.test(this.data.selectedDate))) {
       wx.showToast({
         title: '请输入生日时辰',
         icon: 'error',
@@ -178,100 +67,26 @@ Page({
       })
       return
     }
-    
-
-    this.setData({
-      baziStr: '',
-      missingWuXing: ''
-    })
-
-    let birth = ''
-    if (this.data.calendarType === '农历') {
-      const lunar = this.data.birthStr
-      const {
-        year,
-        month,
-        day,
-        hour,
-        minute
-      } = this.data
-      const lsr = lunisolar.fromLunar({
-        year,
-        month,
-        day,
-        hour,
-        minute,
-        isLeapMonth: false
-      })
-      birth = lsr.format('YYYY-MM-DD HH:mm')
-      console.log(`异步打印----农历: `, lunar)
-      console.log(`异步打印----农历转阳历: `, birth)
-    } else {
-      console.log(`异步打印----阳历: `, this.data.birthStr)
-
-      birth = this.data.birthStr
-    }
-
-    const res = analyzeWuXing(birth)
-    console.log(`异步打印----res: `, res)
-    const baziStr = res.pillars.join(' ')
-    const missingWuXing = res.missingWuXing.length === 0 ? '无' : res.missingWuXing.join('、')
-
-
-    const wuxingInfo = res.wuXingDistribution
-    const wuxingData = [
-      { value: wuxingInfo['金'], itemStyle: { color: '#F5F0E6' } }, // 红色柱子
-      { value: wuxingInfo['木'], itemStyle: { color: '#2D5A3D' } }, // 绿色柱子
-      { value: wuxingInfo['水'], itemStyle: { color: '#2E3192' } }, // 蓝色柱子
-      { value: wuxingInfo['火'], itemStyle: { color: '#C41E3A' } }, // 蓝色柱子
-      { value: wuxingInfo['土'], itemStyle: { color: '#A57865' } } // 紫色柱子
-    ].map((item) => {
-      return {
-        ...item,
-        label: {
-          show: true,
-          position: 'top',
-          color: '#666',
-          fontSize: 14,
-          formatter: '{c}' // 显示 "数值 + 件"
-        }
-      }
-    })
-    chart.setOption({
-      series: [{
-        data: wuxingData,
-        type: 'bar'
-      }]
-    })
-    console.log(`异步打印----baziStr123: `, baziStr)
-    console.log(`异步打印----baziStr123: `, missingWuXing)
-
-
 
     wx.showLoading({
       title: '查询中...',
     })
     setTimeout(() => {
-
-      this.setData({
-        baziStr: baziStr,
-        missingWuXing
-      })
       wx.hideLoading()
 
+
+      const { calendarType, selectedDate, chineseHour } = this.data
+      wx.navigateTo({
+        url: `/pages/result-detial/index?calendarType=${calendarType}&selectedDate=${selectedDate}&chineseHour=${chineseHour}`
+      })
+        
     }, 1000);
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-    setTimeout(() => {
-      // 获取 chart 实例的方式
-      console.log(chart)
-      // const ecIns = this.data.ec.onInit()
-    }, 2000);
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -312,29 +127,9 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-  return {
-    title: '查看你的八字五行',
-    path: '/pages/index/index',
-    success: (res) => {
-      // 分享成功
-      this.setData({
-        shareSuccess: true
-      });
-      // wx.showToast({
-      //   title: '分享成功',
-      //   icon: 'success',
-      //   duration: 2000
-      // });
-      console.log('分享成功一次', res);
-    },
-    fail: (res) => {
-      // 分享失败
-      wx.showToast({
-        title: '分享失败',
-        icon: 'error',
-        duration: 2000
-      });
+    return {
+      title: '查看你的八字五行',
+      path: '/pages/index/index'
     }
-  };
-}
+  }
 })
